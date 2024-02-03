198f937cce361230102ac4555c84c7504f6ca2c3
Stepaniuk-Den <stepaniuk.den@gmail.com>
2024-02-03 09:32:37 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { lazy, useEffect, useState } from "react";
-import { Route, Routes, Navigate, useNavigate } from "react-router-dom";
+import React, { lazy } from "react";
+// import React, { lazy, useEffect, useState } from "react";
+// import { Route, Routes, Navigate, useNavigate } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 
-import Sidebar from "./components/Sidebar/Sidebar";
+// import Sidebar from "./components/Sidebar/Sidebar";
 import Layout from "./components/Layout/Layout";
 
 const Dashboard = lazy(() => import("./components/Dashboard"));
@@ -12,74 +14,74 @@ const Promote = lazy(() => import("./components/Promote"));
 const Help = lazy(() => import("./components/Help"));
 
 function App() {
-  const categories = [
-    "Dashboards",
-    "Product",
-    "Customers",
-    "Income",
-    "Promote",
-    "Help",
-  ];
-  const [selectedCategory, setSelectedCategory] = useState("");
-  const navigate = useNavigate();
+  // const categories = [
+  //   "Dashboards",
+  //   "Product",
+  //   "Customers",
+  //   "Income",
+  //   "Promote",
+  //   "Help",
+  // ];
+  // const [selectedCategory, setSelectedCategory] = useState("");
+  // const navigate = useNavigate();
 
-  const handleSelectedCategory = (category) => {
-    setSelectedCategory(category);
-  };
+  // const handleSelectedCategory = (category) => {
+  //   setSelectedCategory(category);
+  // };
 
-  const getCategoryComponent = (category) => {
-    switch (category) {
-      case "Dashboards":
-        return <Dashboard />;
-      case "Product":
-        return <Product />;
-      case "Customers":
-        return <Customers />;
-      case "Income":
-        return <Income />;
-      case "Promote":
-        return <Promote />;
-      case "Help":
-        return <Help />;
-      default:
-        return null;
-    }
-  };
+  // const getCategoryComponent = (category) => {
+  //   switch (category) {
+  //     case "Dashboards":
+  //       return <Dashboard />;
+  //     case "Product":
+  //       return <Product />;
+  //     case "Customers":
+  //       return <Customers />;
+  //     case "Income":
+  //       return <Income />;
+  //     case "Promote":
+  //       return <Promote />;
+  //     case "Help":
+  //       return <Help />;
+  //     default:
+  //       return null;
+  //   }
+  // };
 
-  useEffect(() => {
-    if (!selectedCategory) {
-      navigate("/");
-    }
-  }, [selectedCategory, navigate]);
+  // useEffect(() => {
+  //   if (!selectedCategory) {
+  //     navigate("/");
+  //   }
+  // }, [selectedCategory, navigate]);
 
   return (
-    <div className="page_container">
-      <Sidebar
-        categories={categories}
-        onSelectedCategory={handleSelectedCategory}
-        selectedCategory={selectedCategory}
-      />
-      <div className="page_category">
-        <p className="page_category_greeting">Hello Evano 👋🏼,</p>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Layout />}></Route>
-            {/* <Route index element={<Dashboard />} /> */}
-            <Route
-              path="/:category"
-              element={getCategoryComponent(selectedCategory)}
-            ></Route>
-            {/* <Route path="/Dashboards" element={<Dashboard />} />
-            <Route path="/Product" element={<Product />} />
-            <Route path="/Customers" element={<Customers />} />
-            <Route path="/Income" element={<Income />} />
-            <Route path="/Promote" element={<Promote />} />
-            <Route path="/Help" element={<Help />} /> */}
-          </Route>
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </div>
-    </div>
+    // <div className="page_container">
+    //   <Sidebar
+    //     categories={categories}
+    //     onSelectedCategory={handleSelectedCategory}
+    //     selectedCategory={selectedCategory}
+    //   />
+    //   <div className="page_category">
+    //     <p className="page_category_greeting">Hello Evano 👋🏼,</p>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Layout />}></Route>
+        {/* <Route index element={<Dashboard />} /> */}
+        {/* <Route
+          path="/:category"
+          element={getCategoryComponent(selectedCategory)}
+        ></Route> */}
+        <Route path="/Dashboards" element={<Dashboard />} />
+        <Route path="/Product" element={<Product />} />
+        <Route path="/Customers" element={<Customers />} />
+        <Route path="/Income" element={<Income />} />
+        <Route path="/Promote" element={<Promote />} />
+        <Route path="/Help" element={<Help />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+    //   </div>
+    // </div>
   );
 }
 
