5e665db0599a5a6d62d795fc491d35a4434feee4
Stepaniuk-Den <stepaniuk.den@gmail.com>
2024-02-03 16:50:24 +0200

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,13 @@
 import React, { Suspense } from "react";
 import Loader from "../Loader/Loader";
-import { Outlet } from "react-router-dom";
+// import { Outlet } from "react-router-dom";
+import MainPage from "../../pages/MainPage";
 
 const Layout = () => {
   return (
     <>
       <Suspense fallback={<Loader />}>
-        <Outlet />
+        <MainPage />
       </Suspense>
     </>
   );
