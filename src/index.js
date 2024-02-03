be68c6b9e3751ca5aeb9d5658eefbbc56b6cbbc4
Stepaniuk-Den <stepaniuk.den@gmail.com>
2024-02-03 17:15:15 +0200

    update

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./sass/index.scss";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-// import { HashRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <BrowserRouter>
-  <BrowserRouter basename="/dashboard">
-    {/* <HashRouter> */}
+  // <BrowserRouter basename="/dashboard">
+  <HashRouter>
     {/* <React.StrictMode> */}
     <App />
     {/* </React.StrictMode> */}
-    {/* </HashRouter> */}
-  </BrowserRouter>
+  </HashRouter>
+  // </BrowserRouter>
 );
